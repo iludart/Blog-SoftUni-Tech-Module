82f887afcbc0f23bf72c493da99fc43043f66e5c
iludart <peter.penev@hotmail.com>
2016-11-25 11:52:30 +0200

    initial commit

---
+++ BlogSystem.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogSystem.WebApp.Startup))]
+namespace BlogSystem.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
